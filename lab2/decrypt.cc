2fe0e70c6e7b2196bfeaa804500b4d0f015c2965
Tommy Lindman <tomli962@student.liu.se>
2015-10-06 10:01:36 +0200

    Fixat med lab2, klar??

--- lab2/decrypt.cc
+++ lab2/decrypt.cc
@@ -22,7 +22,8 @@ class key_hash
 
       for (int i{N - 1}; i >= 0; i--)
 	{
-	  hash +=  KEYbit(x, i) * pow(2.0, i);
+	  hash = hash | KEYbit(x, i) << i;
+	  //hash +=  KEYbit(x, i) * pow(2.0, i);
 	}
       return hash;
     }
@@ -96,10 +97,10 @@ unordered_map < Key, vector< vector< int > >, key_hash > getMap(Key T[], int ran
   unordered_map < Key, vector< vector< int > >, key_hash > temp;
 
   //Populate one half of the hash table
-  for( int i = {0}; i < permutations.size(); ++i)
+  for(unsigned int i = {0}; i < permutations.size(); ++i)
     {
       Key subSet{{0}};
-      for( int j = {0}; j < permutations[i].size(); ++j)
+      for(unsigned int j = {0}; j < permutations[i].size(); ++j)
 	{
 	  subSet = subSet + T[ permutations[i][j] ];
 	 
@@ -123,10 +124,10 @@ int
 main(int argc, char* argv[])
 {
   unsigned char buffer[C+1];     // temporary string buffer
-  Key candidate = {{0}};         // a password candidate
+  //Key candidate = {{0}};         // a password candidate
   Key encrypted;                 // the encrypted password
-  Key candenc;                   // the encrypted password candidate
-  Key zero = {{0}};              // the all zero key
+  //Key candenc;                   // the encrypted password candidate
+  //Key zero = {{0}};              // the all zero key
   Key T[N];                      // the table T
 
   if (argc != 2)
@@ -141,8 +142,11 @@ main(int argc, char* argv[])
   // read in table T
   for (int i{0}; i < N; ++i)
     {
-       scanf("%s", buffer);
-       T[i] = KEYinit(buffer);
+      if ( scanf("%s", buffer) != 1  )
+	{
+	  cerr << "Error reading file." << endl;
+	}
+      T[i] = KEYinit(buffer);
      }
 
    auto begin = chrono::high_resolution_clock::now();
@@ -155,10 +159,13 @@ main(int argc, char* argv[])
    int n = 0;
    int m = 0;
 
+   int toSubtract = 0;
+
    if (N%2 != 0)
      {
        n = N/2;
        m=N/2+1;
+       toSubtract = -1;
      }
    else
      {
@@ -167,29 +174,58 @@ main(int argc, char* argv[])
      }
 
   first_half = getMap(T, n, 0);
-  second_half = getMap(T, m, m);
+  second_half = getMap(T, m, m + toSubtract);
 
-  
-   for ( auto ff = first_half.begin(); ff != first_half.end(); ++ff )
+  // Check cands in first_half
+  if( first_half.count(encrypted) == 1 )
+    {
+      //cout << "We have a cand in first_half" << endl;
+      for( unsigned int i = 0; i < first_half[encrypted].size(); ++i )
+	{
+	  bitset<N> b;
+	  for( unsigned int j = 0; j < first_half[encrypted][i].size(); ++j )
+	    {
+	      b.set( first_half[encrypted][i][j] );
+	    }
+	  BitToString(b);
+	}
+    }
+
+  // Check cands in second_half
+  if( second_half.count(encrypted) == 1 )
+    {
+      for( unsigned int i = 0; i < second_half[encrypted].size(); ++i )
+	{
+	  bitset<N> b;
+	  for( unsigned int j = 0; j < second_half[encrypted][i].size(); ++j )
+	    {
+	      b.set( second_half[encrypted][i][j] );
+	    }
+	  BitToString(b);
+	}
+    }
+
+  // Check if we have any pwd candidates in first and second combined
+  for ( auto ff = first_half.begin(); ff != first_half.end(); ++ff )
     {
 
       Key temp = encrypted - ff->first;
       if ( second_half.count(temp) == 1 )
 	{
 	  bitset<N> b;
-	  for(int fi = 0; fi < ff->second.size(); fi++)
+	  for(unsigned int fi = 0; fi < ff->second.size(); fi++)
 	    {
 	      bitset<N> b;
 	      
-	      for(int fj = 0; fj < ff->second[fi].size(); fj++)
+	      for(unsigned int fj = 0; fj < ff->second[fi].size(); fj++)
 		{
 		  b.set(ff->second[fi][fj]);
 		}
 
-	      for( int si = {0}; si < second_half[temp].size(); ++si)
+	      for(unsigned int si = {0}; si < second_half[temp].size(); ++si)
 		{
 		  bitset<N> first_part = b;
-		  for(int sj = {0}; sj < second_half[temp][si].size(); ++sj)
+		  for(unsigned int sj = {0}; sj < second_half[temp][si].size(); ++sj)
 		    {
 		      first_part.set(second_half[ temp ][si][sj] );
 		    }
